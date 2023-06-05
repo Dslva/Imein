5e4e2f4432e193634b9ed992f2e79a94f71e31f9
slva <diego.slvasm@gmail.com>
2023-06-04 23:44:06 -0400

    tab menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,19 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'f-password',
+    loadChildren: () => import('./f-password/f-password.module').then( m => m.FPasswordPageModule)
+  },
+  
 ];
 
 @NgModule({
--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,24 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+    children:[
+      {
+        path: 'qr-code',
+        loadChildren: () => import('./.././qr-code/qr-code.module').then( m => m.QrCodePageModule)
+      },
+      {
+        path: 'buscar',
+        loadChildren: () => import('./.././buscar/buscar.module').then( m => m.BuscarPageModule)
+      },
+      {
+        path: 'ajustes',
+        loadChildren: () => import('./.././ajustes/ajustes.module').then( m => m.AjustesPageModule)
+      },
+      {
+        path: 'ingresar',
+        loadChildren: () => import('./.././ingresar/ingresar.module').then( m => m.IngresarPageModule)
+      },
+    ] 
   }
 ];
 
